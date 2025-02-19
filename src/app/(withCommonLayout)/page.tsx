580620331fe52e4fa3a25fc231e8b50a9ddf2fc7
PRONOY <pronoybanik82@gmail.com>
2025-02-19 22:31:04 +0600

    implemnt context api

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
+import { useUser } from "@/context/UserContext";
 
 const HomePage = () => {
+  const user = useUser();
+  console.log(user);
 
   return <div>Well come home page</div>;
 };
